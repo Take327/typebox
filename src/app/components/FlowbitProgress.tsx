bcadaf284a0a88e9bc1682701d52fae82718b129
Take327 <gatewayseed2@gmail.com>
2025-01-04 02:35:26 +0900

    ダークモード対応解除

--- src/app/components/FlowbitProgress.tsx
+++ src/app/components/FlowbitProgress.tsx
@@ -4,7 +4,7 @@ import { Flowbite, Progress } from "flowbite-react";
 const customTheme: CustomFlowbiteTheme = {
   progress: {
     base: "w-full overflow-hidden rounded-full bg-gray-200",
-    label: "mb-1 flex justify-between font-medium dark:text-white",
+    label: "mb-1 flex justify-between font-medium",
     bar: "space-x-2 rounded-full text-center font-medium leading-none text-white",
     color: {
       a8d8cb: "bg-[#a8d8cb]",
