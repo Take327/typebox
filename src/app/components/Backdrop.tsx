02e08d7e6ac9f40e02f110eb29d4a6e96830d99b
Take327 <gatewayseed2@gmail.com>
2025-01-05 10:36:21 +0900

    Spinner修正

--- src/app/components/Backdrop.tsx
+++ src/app/components/Backdrop.tsx
@@ -1,6 +1,54 @@
 import React from "react";
-import { Spinner } from "flowbite-react";
+import { CustomFlowbiteTheme, Flowbite, Spinner } from "flowbite-react";
 import { useProcessing } from "../../context/ProcessingContext";
+
+const customTheme: CustomFlowbiteTheme = {
+  spinner: {
+    base: "inline animate-spin text-gray-200",
+    color: {
+      failure: "fill-red-600",
+      gray: "fill-gray-600",
+      info: "fill-cyan-600",
+      pink: "fill-pink-600",
+      purple: "fill-purple-600",
+      success: "fill-green-500",
+      warning: "fill-yellow-400",
+    },
+    light: {
+      off: {
+        base: "dark:text-gray-600",
+        color: {
+          failure: "",
+          gray: "dark:fill-gray-300",
+          info: "",
+          pink: "",
+          purple: "",
+          success: "",
+          warning: "",
+        },
+      },
+      on: {
+        base: "",
+        color: {
+          failure: "",
+          gray: "",
+          info: "",
+          pink: "",
+          purple: "",
+          success: "",
+          warning: "",
+        },
+      },
+    },
+    size: {
+      xs: "h-3 w-3",
+      sm: "h-4 w-4",
+      md: "h-6 w-6",
+      lg: "h-8 w-8",
+      xl: "h-10 w-10",
+    },
+  },
+};
 
 const Backdrop: React.FC = () => {
   const { isProcessing } = useProcessing();
@@ -9,7 +57,9 @@ const Backdrop: React.FC = () => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <Spinner size="xl" aria-label="処理中..." />
+      <Flowbite theme={{ theme: customTheme }}>
+        <Spinner aria-label="Extra large spinner example" size="xl" />
+      </Flowbite>
     </div>
   );
 };
