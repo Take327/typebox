add54134aa695bc6467053ff9373c06640076df3
Take327 <gatewayseed2@gmail.com>
2025-02-03 01:41:53 +0900

    スマホスタイルとの調整実施

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ title, children }: { title: string; children: React.ReactNode }) {
   return (
-    <div className="card flex aspect-square flex-col items-start rounded bg-white p-4 shadow-md transition-shadow duration-300 hover:shadow-lg sm:max-h-[600px] sm:max-w-[600px]">
+    <div className="card flex flex-col items-start rounded bg-white p-4 shadow-md transition-shadow duration-300 hover:shadow-lg sm:max-h-[600px] sm:max-w-[600px]">
       <h2 className="mb-1 text-xl font-bold text-gray-800">{title}</h2>
       {children}
     </div>
